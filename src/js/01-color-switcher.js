70bf1856ca99b2c3c311fc5fd2aa1bd73c49d0da
Alona Melnykova <alonamelnykova@gmail.com>
2021-10-05 00:11:56 +0300

    taks 1 completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,21 @@
 const allButtonsNode = document.querySelectorAll('button');
 
-console.log(document.querySelectorAll('button'));
-allButtonsNode.addEventListener('click', e => {
-  document.style.backgroundColor = '#black';
+const startButton = allButtonsNode[0];
+const stopButton = allButtonsNode[1];
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let intervalRepeat = null;
+startButton.addEventListener('click', () => {
+  intervalRepeat = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startButton.setAttribute('disabled', true);
+});
+
+stopButton.addEventListener('click', () => {
+  clearInterval(intervalRepeat);
+  startButton.removeAttribute('disabled');
 });
