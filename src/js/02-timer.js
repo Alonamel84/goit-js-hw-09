514328e81c052c34cc358114d4d2883258a88836
Alona Melnykova <alonamelnykova@gmail.com>
2021-10-06 00:18:56 +0300

    update timer code.

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,29 +1,84 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
-// const styling = document.querySelectorAll('.field');
 
-// {
-//     enableTime: true,
-//     dateFormat: "Y-m-d H:i",
-// }
+const fields = document.querySelectorAll('.field');
+// fields.style.display = 'flex';
+// fields.style.flexDirection = 'column';
+const labels = document.querySelector('.timer');
+labels.style.fontSize = '30px';
+labels.style.color = 'blue';
+
+const allButtonsNode = document.querySelectorAll('button');
+
+const startButton = allButtonsNode[0];
+startButton.style.padding = '10px';
+startButton.style.borderRadius = '4px';
+startButton.style.fontSize = '20px';
+
+const stopButton = allButtonsNode[1];
+stopButton.style.padding = '10px';
+stopButton.style.borderRadius = '4px';
+stopButton.style.fontSize = '20px';
+
+startButton.disabled = true;
+const inputNode = document.querySelector('#datetime-picker');
+let dateToSelect;
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    dateToSelect = selectedDates[0];
+    startButton.setAttribute('disabled', true);
+    if (selectedDates[0] - options.defaultDate <= 0) {
+      return alert('Please choose a date in the future');
+    } else {
+      startButton.removeAttribute('disabled');
+    }
   },
 };
+// let calculatedMs;
+startButton.addEventListener('click', e => {
+  const intervalSet = setInterval(() => {
+    let todayDate = new Date();
+    const calculatedMs = dateToSelect - todayDate;
+    const obj = convertMs(calculatedMs);
+    function addLeadingZero(value) {
+      return String(value).padStart(2, '0');
+    }
 
-const inputNode = document.querySelector('#datetime-picker');
+    const dateNode = document.querySelectorAll('.value');
+    const daysNode = dateNode[0];
+    daysNode.textContent = addLeadingZero(obj.days);
+    const hoursNode = dateNode[1];
+    hoursNode.textContent = addLeadingZero(obj.hours);
+    const minutesNode = dateNode[2];
+    minutesNode.textContent = addLeadingZero(obj.minutes);
+    const secondsNode = dateNode[3];
+    secondsNode.textContent = addLeadingZero(obj.seconds);
+
+    if (calculatedMs <= 0) clearInterval(intervalSet);
+  }, 1000);
+});
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
 flatpickr(inputNode, options);
-// const calculatedDate (startDate, endDate){
-//     startDate = inputNode.Date()
-
-// }
-const today = new Date();
-const selectedDate = inputNode.flatpickr(inputNode, options);
-const calculateDate = new Date(inputNode.value);
-
-console.log(defaultDate);
