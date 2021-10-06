c037e438174812f4486eebe90514a2bd253dea28
Alona Melnykova <alonamelnykova@gmail.com>
2021-10-06 18:34:15 +0300

    3taks completed

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -13,14 +13,14 @@ function createPromise(position, delay) {
   });
 }
 const delayNode = document.querySelector('input[name="delay"]');
-
-const stepNode = Number(document.querySelector('input[name="step"]'));
-const amountNode = Number(document.querySelector('input[name="amount"]'));
-
+const formNode = document.querySelector('form');
+const stepNode = document.querySelector('input[name="step"]');
+const amountNode = document.querySelector('input[name="amount"]');
 const buttonNode = document.querySelector('button');
-buttonNode.addEventListener('submit', e => {
+
+formNode.addEventListener('submit', e => {
   e.preventDefault();
-  console.log(delayNode.value);
+  console.log(delayNode.value, amountNode.value, stepNode.value);
   for (let position = 1; position <= amountNode.value; position++) {
     delay = delayNode.value + stepNode.value;
     createPromise(position, delay)
