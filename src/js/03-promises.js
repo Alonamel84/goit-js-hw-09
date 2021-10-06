f45a8cf7c63fd4ceaee272fbe1ffb65913a413b6
Alona Melnykova <alonamelnykova@gmail.com>
2021-10-06 19:04:00 +0300

    fixed t3. convert to numbers

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -17,12 +17,12 @@ const formNode = document.querySelector('form');
 const stepNode = document.querySelector('input[name="step"]');
 const amountNode = document.querySelector('input[name="amount"]');
 const buttonNode = document.querySelector('button');
-
+let delay = Number(delayNode.value);
 formNode.addEventListener('submit', e => {
   e.preventDefault();
-  console.log(delayNode.value, amountNode.value, stepNode.value);
-  for (let position = 1; position <= amountNode.value; position++) {
-    delay = delayNode.value + stepNode.value;
+
+  // console.log(delayNode.value, amountNode.value, stepNode.value);
+  for (let position = 1; position <= Number(amountNode.value); position++) {
     createPromise(position, delay)
       .then(({ position, delay }) => {
         console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
@@ -30,6 +30,7 @@ formNode.addEventListener('submit', e => {
       .catch(({ position, delay }) => {
         console.log(`❌ Rejected promise ${position} in ${delay}ms`);
       });
+    delay += Number(stepNode.value);
   }
 });
 
